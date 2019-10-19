e5a2877f394dfaa4592adeaf103c19c71487c308
M <tiankongguangzi@gmail.com>
2019-10-19 14:31:09 +0800

    changed the settings page as a test

--- BlindWrite/screens/SettingsScreen.js
+++ BlindWrite/screens/SettingsScreen.js
@@ -1,12 +1,29 @@
 import React from 'react';
 import { ExpoConfigView } from '@expo/samples';
+import {
+  View,
+  Text,
+} from 'react-native';
 
 export default function SettingsScreen() {
   /**
    * Go ahead and delete ExpoConfigView and replace it with your content;
    * we just wanted to give you a quick view of your config.
    */
-  return <ExpoConfigView />;
+  return (
+    <View>
+      <Text>
+        <h1>
+          Settings page
+          Example: 
+            - ShowShape (should we display the shape?)
+            - Vibration mode
+            - Vibration strength
+        </h1>
+      </Text>
+      
+    </View>
+  );
 }
 
 SettingsScreen.navigationOptions = {
